d251b3077a4f4557f6b9bee1527b614d257e6575
Rafael <librerorafael@outlook.com>
2024-03-01 14:15:10 +0100

    Add project files.

---
+++ Data/BoxBoxContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BoxBox.Data
+{
+    public class BoxBoxContext: DbContext
+    {
+        public BoxBoxContext(DbContextOptions<BoxBoxContext> options)
+        :base (options) { }
+
+
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BoxBox.Models
+{
+    [Table("Users")]
+
+    public class User
+    {
+        [Key]
+        [Column("UserID")]
+        public int UserId { get; set; }
+        [Column("UserName")]
+        public string UserName { get; set; }
+        [Column("Email")]
+        public string Email { get; set; }
+        [Column("Password")]
+        public string Password { get; set; }
+        [Column("RegistrationDate")]
+        public DateTime RegistrationDate { get; set; }
+        [Column("LastAccess")]
+        public DateTime LastAccess { get; set; }
+        [Column("RolID")]
+        public int RolId { get; set; }
+        [Column("ProfilePicture")]
+        public string ProfilePicture { get; set; }
+        [Column("TotalPosts")]
+        public int TotalPosts { get; set; }
+
+    }
+}
